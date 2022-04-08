3657a47d3a71ed41ca80fc3292b03ffc0d291d81
Cong Duy <rudbich199@gmail.com>
2022-04-08 09:54:48 +0700

    vuex-2

--- src/store/index.store.js
+++ src/store/index.store.js
@@ -39,5 +39,31 @@ export const store = new Vuex.Store({
     modules: {
         home: homeStore,//home la namespaced
         cart: cartStore,//cart la namespaced
+    },
+    state: {
+        count: 0,
+        homeProducts:[],
+        human: {}
+    },
+    mutations: {
+        increment(state) {
+            state.count++;
+        },
+        decrement(state) {
+            state.count--;
+        },
+        setHomeProducts(state, products) {
+            state.homeProducts = products;
+        },
+        setHuman(state,payload){
+            state.human = payload;
+        }
+    },
+    actions:{
+        async loadHomeProducts({commit}) {
+            const resp = await homeService.getHome();
+            commit('setHomeProducts',resp.data);
+        }
     }
+
 })
